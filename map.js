88169e431516d5edb5a34d21697a0c767b62efab
vejhed61-code <vejhed61@gmail.com>
2026-09-18 11:38:14 -0600

    Build Custom Leaflet map for Lab 4

--- map.js
+++ map.js
@@ -1,1 +1,23 @@
-// Build your map here!
+let map = L.map("map", {center: [32.7667, -108.2667], zoom: 12});
+L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {attribution: '&copy; OpenStreetMap contributors'}
+).addTo(map);
+let pnt = L.marker([32.7, -108.3]).addTo(map);
+pnt.bindPopup("<b>Tyrone</b> is a nearby community southwest of Silver City.");
+let line = L.polyline([
+    [32.7, -108.3],
+    [32.75, -108.1333]
+], {
+    color: "blue",
+    weight: 5
+}).addTo(map);
+let area = L.polygon([
+    [32.7667, -108.2839],
+    [32.85, -108.2167],
+    [32.75, -108.1333]
+], {
+    color: "green",
+    fillColor: "lightgreen"
+}).addTo(map);
+L.control.scale({position: "bottomright"}).addTo(map);
